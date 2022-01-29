e9aa2e6d265dd0352d89590d84a259697ce69497
tuliotroche <tgtroche@gmail.com>
2022-01-29 11:36:32 -0500

    random cloud generation added

--- pages/components/Cloud.tsx
+++ pages/components/Cloud.tsx
@@ -5,13 +5,17 @@ interface CloudProps {
     size: string,
     color: string,
     speed: string,
+    opacity: number,
+    top: number,
 }
 
-function Cloud({size, color, speed}: CloudProps): JSX.Element {
+function Cloud({size, color, speed, opacity, top}: CloudProps): JSX.Element {
     const style = {
         '--size': size,
         '--color': color,
-        '--speed': speed
+        '--speed': speed,
+        '--cloud-opacity': opacity,
+        '--top': top,
     } as React.CSSProperties;
 
     return <div
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,33 @@ import Cloud from "./components/Cloud";
 import React from "react";
 
 const Home: NextPage = () => {
-  return (
-    <main className={styles.main}>
-        <Cloud size={'.5px'} color={'whitesmoke'} speed={'20s'} />
-        <Cloud size={'2px'} color={'whitesmoke'} speed={'10s'}/>
-        <Cloud size={'3px'} color={'whitesmoke'} speed={'7s'}/>
+
+    function generateClouds() {
+        let clouds = [];
+
+        for (let i = 0; i < 10; i++) {
+            let randomSeed = Math.floor(Math.random() * 100)* .01;
+            clouds.push(
+                <Cloud
+                    key={i}
+                    size={`${randomSeed}px`}
+                    color={'whitesmoke'}
+                    speed={`${randomSeed * 15}s`}
+                    opacity={randomSeed}
+                    top={randomSeed * 4.5}
+                />
+            );
+        }
+        return clouds;
+    }
+
+    const clouds = generateClouds();
+
+    return (
+    <main className={styles.main} style={{overflowX: "hidden"}}>
+        {clouds.map(cloud => cloud)}
     </main>
-  )
+    )
 }
 
 export default Home
